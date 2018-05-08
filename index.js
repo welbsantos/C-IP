e80c71b7582a504103ce5cc0b30177aa3dbd2860
Liam <lgaskell@freewebstore.com>
2018-05-08 15:33:29 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const electron = require('electron');
+const { app, Tray, Menu, BrowserWindow } = electron;
+const path = require('path');
+const iconPath = path.join(__dirname, 'icon.png');
+let appIcon = null;
+let win = null;
+
+const ip = require('ip');
+const publicIp = require('public-ip');
+var ExternalIP;
+var ncp = require("copy-paste");
+
+app.on('ready', () => {
+    publicIp.v4().then(eip => {
+        ExternalIP = eip;
+        console.log(ExternalIP);
+        win = new BrowserWindow({show: false});
+        appIcon = new Tray(iconPath);
+        var contextMenu = Menu.buildFromTemplate([
+            {
+            label: "Copy WLAN: " + ip.address("public"),
+            click: function() {
+                ncp.copy(ip.address("public"));
+                }
+            },
+            {
+            label: "Copy Public: " + ExternalIP,
+            click: function() {
+                ncp.copy(ExternalIP);
+                }
+            },
+            { label: 'Quit',
+            accelerator: 'CommandOrControl+Q',
+            selector: 'terminate:',
+            click: function() {
+                    app.quit();
+                    app.exit();
+                }
+            }
+        ]);
+        appIcon.setToolTip('View your IP information');
+        appIcon.setContextMenu(contextMenu);
+    });
+})
+app.on('quit', () => {
+    app.quit();
+    app.exit();
+})
